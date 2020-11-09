d7ab758ddd6f8345c456aa29cb1378e446a0166e
aranmani <67229307+aranmani@users.noreply.github.com>
2020-11-08 19:47:53 -0600

    Update Config with insta id

--- data/config.js
+++ data/config.js
@@ -1,32 +1,32 @@
 module.exports = {
   // Name of your site. Can be the name of a brand, or your personal name.
-  title: 'Gatsby Starter',
+  title: 'Lazylicious 2020',
   // What's the purpose of this website? What can we expect from it?
   description:
-    `A portfolio based on your latest Instagram posts, implemented with Uber's Base Web Design System.`,
+    `A portfolio based on my latest Instagram posts, implemented with Uber's Base Web Design System.`,
   // Your legal name.
   legalName: 'Gatsby Starter',
   // URL to this website. If you bought `www.domain.com`, then it will be `www.domain.com`.
-  url: 'https://www.robonomy.com',
+  url: '',
   // Favicon that will display on browsers.
   logo: 'static/images/favicon.png',
   // Who made this website?
-  author: 'Juan Alejandro Morais',
+  author: 'Aran Mani LLC',
   // What's the website of the author?
-  authorUrl: 'https://www.timrodz.com',
+  authorUrl: 'https://github.com/aranmani',
   // Your social media presence, in the form of usernames.
   social: {
-    // tells the Instagram API the username to get posts from. 
+    // tells the Instagram API the username to get posts from.
     // If the url is `https://www.instagram.com/natgeotravel`, your username is `natgeotravel`.
-    instagram: 'natgeotravel', 
+    instagram: 'lazylicious2020',
     // Optional
-    twitter: '@NatGeoTravel',
+    twitter: '@lazylicious2020',
   },
   socialLinks: {
     // Complete URL verison of `social.instagram`.
-    instagram: 'https://www.instagram.com/natgeotravel',
+    instagram: 'https://www.instagram.com/lazylicious2020',
     // Optional
-    twitter: 'https://twitter.com/NatGeoTravel',
+    twitter: 'https://twitter.com/lazylicious2020',
   },
   // If applicable, your Google Analytics crawl ID.
   googleAnalyticsId: 'UA-XXXXXXXX-X',
@@ -41,7 +41,7 @@ module.exports = {
   // Basic contact details.
   contact: {
     // Email address to contact you.
-    email: 'timrodz@icloud.com',
+    email: '',
   },
   // When was this website created?
   foundingDate: '2020',
